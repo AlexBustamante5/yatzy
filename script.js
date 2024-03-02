d1395b43e425ec44eca64d9d7052272a4f11b40e
Alex Bustamante <90732151+AlexBustamante5@users.noreply.github.com>
2024-03-02 18:43:13 -0500

    Updated code to only roll unselected dice

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 document.getElementById('roll-dice').addEventListener('click', function() {
-    let results = rollDice(5); // Rolls 5 dice
-    displayResults(results);
+    rollNotSelectedDice();
 });
 
 document.getElementById('reset-dice').addEventListener('click', function() {
-    resetDice(); // resets dice when the reset-dice button is clicked
+    resetDice();
+    resetSelections();
 });
 
-function rollDice(numberOfDice) {
+/*function to roll the dics */
+function rollDice(numberOfDice, keep) {
     let rolls = [];
     for (let i = 0; i < numberOfDice; i++) {
-        rolls.push(Math.floor(Math.random() * 6) + 1); // Rolls a single die and adds result to rolls array
+        if (!keep[i]) {
+            rolls.push(Math.floor(Math.random() * 6) + 1); // Rolls a single die
+        } else {
+            rolls.push(null); // Keeps the dice
+        }
     }
     return rolls;
 }
 
-function displayResults(rolls) {
-    rolls.forEach((result, index) => {
-        const diceElement = document.querySelector(`#dice-${index + 1} span`);
-        diceElement.textContent = result; // Updates each dice display
-    });
+/*only roll not selected dice */
+function rollNotSelectedDice() {
+    const keep = [];
+    for (let i = 1; i <= 5; i++) {
+        const checkbox = document.getElementById(`keep-${i}`);
+        keep.push(checkbox.checked);
+    }
+    let results = rollDice(5, keep);
+    displayResults(results, keep);
 }
 
+function displayResults(rolls, keep) {
+    rolls.forEach((result, index) => {
+        if (result !== null) {
+            const diceElement = document.getElementById(`dice-${index + 1}`);
+            diceElement.firstChild.textContent = result; // Updates only the text node
+        }
+    });
+}
+/*clear all dice*/
 function resetDice() {
-    for (let i = 0; i <= 5; i++) {
-        const diceElement = document.querySelector(`#dice-${i + 1} span`);
-        diceElement.textContent = ''; // Clears each dice display
+    for (let i = 1; i <= 5; i++) {
+        const diceElement = document.getElementById(`dice-${i}`);
+        diceElement.firstChild.textContent = '0'; // Resets each dice display
+    }
+}
+/*uncheck the checkboxes*/
+function resetSelections() {
+    for (let i = 1; i <= 5; i++) {
+        const checkbox = document.getElementById(`keep-${i}`);
+        checkbox.checked = false;
     }
 }
